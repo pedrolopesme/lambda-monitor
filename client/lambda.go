c780b5cc237d83a03e97250dd7fe66d3be7a01c2
pedro.mendes <pedro.mendes@corp.globo.com>
2018-07-04 10:12:50 -0300

    client/lambda: move laws lambda client to its own package

---
+++ client/lambda.go
@@ -0,0 +1,18 @@
+package client
+
+import "github.com/aws/aws-sdk-go/service/lambda/lambdaiface"
+
+// TODO add comments
+type lambdaClient struct {
+	client lambdaiface.LambdaAPI
+}
+
+func (lc lambdaClient) GetClient() lambdaiface.LambdaAPI {
+	return lc.client
+}
+
+func NewLambdaClient(client lambdaiface.LambdaAPI) *lambdaClient {
+	return &lambdaClient{
+		client: client,
+	}
+}

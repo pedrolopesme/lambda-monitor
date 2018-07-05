e45a400763f3f712ece2d10062231f1dcab4d1d6
pedro.mendes <pedro.mendes@corp.globo.com>
2018-07-05 10:10:06 -0300

    core/lambda: add fields and funcs to Lambda struct

--- core/lambda.go
+++ core/lambda.go
@@ -1,5 +1,34 @@
 package core
 
+import (
+	"github.com/aws/aws-sdk-go/service/lambda"
+	"time"
+)
+
 type Lambda struct {
-	Name string
+	name          string
+	lastModified  time.Time
+	codeSha256    string
+	codeSize      uint32
+	description   string
+	variables     map[string]string
+	arn           string
+	handler       string
+	revisionId    string
+	role          string
+	runtime       string
+	timeout       uint32
+	tracingConfig map[string]string
+	version       string
+}
+
+func (l Lambda) GetName() string {
+	return l.name
+}
+
+// TODO add tests
+func LambdaBuilder(awsFunction *lambda.FunctionConfiguration) (lambda *Lambda) {
+	return &Lambda{
+		name: *awsFunction.FunctionName,
+	}
 }

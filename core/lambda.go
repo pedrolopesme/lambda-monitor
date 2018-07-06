e9f66ca8f2355458f50b2cbd46f9ed55266c7741
pedro.mendes <pedro.mendes@corp.globo.com>
2018-07-06 10:26:31 -0300

    client/lambda: add Lambdas and LambdasIterator structs and related funcs

--- core/lambda.go
+++ core/lambda.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+// Lambda represents a AWS Lambda with all
+// properties need by lambda-monitor to run
 type Lambda struct {
 	name          string
 	lastModified  time.Time
@@ -22,13 +24,72 @@ type Lambda struct {
 	version       string
 }
 
+// GetName allows others to retrieve Lambda name
+// TODO add tests
 func (l Lambda) GetName() string {
 	return l.name
 }
 
+// LambdaBuilder knows how to build a Lambda from a FunctionConfiguration
+// provided by AWS package
 // TODO add tests
 func LambdaBuilder(awsFunction *lambda.FunctionConfiguration) (lambda *Lambda) {
 	return &Lambda{
 		name: *awsFunction.FunctionName,
 	}
+}
+
+// Lambdas stores a collection of Lambdas
+// and collection related funcs to operate over them
+type Lambdas struct {
+	lambdas []Lambda
+}
+
+// Get returns the internal array of lambdas from Lambdas type
+// TODO add tests
+func (lambdas Lambdas) Get() []Lambda {
+	return lambdas.lambdas
+}
+
+// Set replaces the internal array of lambdas
+// TODO add tests
+func (lambdas Lambdas) Set(newLambdas []Lambda) {
+	lambdas.lambdas = newLambdas
+}
+
+// GetIterator retrieves an iterator to the
+// current Lambdass
+// TODO add tests
+func (lambdas Lambdas) GetIterator() *LambdasIterator {
+	return &LambdasIterator{
+		lambdas: lambdas,
+		head:    0,
+	}
+}
+
+// LambdasIterator contains a temporary data
+// to iterate over a Lambdas type
+type LambdasIterator struct {
+	lambdas Lambdas
+	head    int
+}
+
+// Next moves the internal head of the iterator to the
+// next position if its possible
+// TODO add tests
+func (iterator LambdasIterator) Next() bool {
+	if iterator.head < len(iterator.lambdas.Get())-1 {
+		iterator.head++
+		return true
+	}
+	return false
+}
+
+// Retrieves the lambda stored in the current head position
+// TODO add tests
+func (iterator LambdasIterator) Get() (lambda Lambda) {
+	if iterator.head < len(iterator.lambdas.Get()) {
+		lambda = iterator.lambdas.Get()[iterator.head]
+	}
+	return
 }

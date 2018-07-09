78a356d7ef1403900ea083aa57086254c3516c94
pedro.mendes <pedro.mendes@corp.globo.com>
2018-07-09 09:45:44 -0300

    core/test: add utilities for testing

---
+++ core/test.go
@@ -0,0 +1,31 @@
+package core
+
+import (
+	"os"
+	"bytes"
+	"io"
+)
+
+// Captures the output by replacing the
+// standard stdout with a buffer and then restore it again.
+// Returns the buffer contents as String
+func captureOutput(f func()) string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f()
+
+	w.Close()
+	os.Stdout = old
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
+}
+
+func buildMockedLambda(name string) *Lambda {
+	return &Lambda{
+		name: name,
+	}
+}

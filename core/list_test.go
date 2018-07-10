dd4bf65a12f4ab237b9bd5b32edb8d3879ddcbbd
pedro.mendes <pedro.mendes@corp.globo.com>
2018-07-10 09:57:25 -0300

    core/list_test: add tests to Print() func

--- core/list_test.go
+++ core/list_test.go
@@ -12,3 +12,33 @@ func TestPrintLambda(test *testing.T) {
 
 	assert.Equal(test, "Dummy\n", output)
 }
+
+func TestPrintLambdasWhenTheresNoLambda(test *testing.T) {
+	lambdas := Lambdas{}
+	output := captureOutput(func() {
+		lambdas.Print()
+	})
+	assert.Equal(test, "No Lambdas Found\n", output)
+}
+
+func TestPrintLambdasWhenThereIsOneLambda(test *testing.T) {
+	lambdas := Lambdas{}
+	lambdas.Put(*buildMockedLambda("one"))
+
+	output := captureOutput(func() {
+		lambdas.Print()
+	})
+	assert.Equal(test, "one\n", output)
+}
+
+func TestPrintLambdasWhenThereAreMultipleLambda(test *testing.T) {
+	lambdas := Lambdas{}
+	lambdas.Put(*buildMockedLambda("one"))
+	lambdas.Put(*buildMockedLambda("two"))
+	lambdas.Put(*buildMockedLambda("three"))
+
+	output := captureOutput(func() {
+		lambdas.Print()
+	})
+	assert.Equal(test, "one\ntwo\nthree\n", output)
+}
